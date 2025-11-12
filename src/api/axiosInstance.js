c4ae82c55b8450a1bc6fb0c134c69915efa90fd1
jo-oseeph <realsejjo2001@gmail.com>
2025-11-12 12:48:35 +0300

    12th Nov 2025

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://cybex-backend.onrender.com/api",
+  baseURL: "https://cybex-backend.onrender.com/api" || "http://localhost:5000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // Attach token automatically
