c59d0d134da49c2f952bf2d6251fa13eb6eb2c77
jo-oseeph <realsejjo2001@gmail.com>
2026-04-21 18:41:00 +0300

    axios

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://cybex-backend.onrender.com/api" || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
